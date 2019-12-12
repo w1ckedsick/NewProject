7204da4d1c6d99ecaed1d268a529077a7107a4b4
lena390 <lunguev@yandex.ru>
2019-12-12 15:22:38 +0300

    commit

--- src/LinearSearch.java
+++ src/LinearSearch.java
@@ -1,22 +1,18 @@
-import java.util.Arrays;
-
 public class LinearSearch {
     public static void main(String[] args) {
         int[] array = {1, 2, 3, 4, 5, 6, 7, 8, 9};
         int k = 6;
 
-        int newLength = array.length + 1;
+        int newLength = array.length + 1; //копирует массив
         int[] aCopy = new int[newLength];
         System.arraycopy(array, 0, aCopy, 0, newLength - 1);
-        aCopy[newLength - 1] = k;
 
-        //System.out.println(Arrays.toString(aCopy));
-
-        int i = 0;
+        int i = 0; //поиск
         while (aCopy[i] != k) {
             i++;
         }
         int x = (i == newLength - 1) ? -1 : i;
+
         System.out.println(x);
     }
 }
